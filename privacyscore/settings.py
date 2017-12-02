85ab8d72fcea2afcb04628086276054ccaa24036
Waleed Iqbal <waleediqbal57@gmail.com>
2017-12-02 21:42:42 +0100

    minor changes

--- privacyscore/settings.py
+++ privacyscore/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dev only!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Fasle
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
